ff7f2110afc18f7d0f3b71bab42eaecc6c3290d0
riket.bhosale <riket.bhosale@godigit.com>
2025-08-15 12:06:07 +0530

    first commit

---
+++ src/main/java/org/godigit/policyvault/PolicyVaultApplication.java
@@ -0,0 +1,13 @@
+package org.godigit.policyvault;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PolicyVaultApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PolicyVaultApplication.class, args);
+	}
+
+}
---
+++ src/test/java/org/godigit/policyvault/PolicyVaultApplicationTests.java
@@ -0,0 +1,13 @@
+package org.godigit.policyvault;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PolicyVaultApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
