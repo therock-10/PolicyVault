0efb3021558bd9f01a8bf781b481304c2a190c4d
konu2514 <manjunathkonu@gmail.com>
2025-08-22 18:30:29 +0530

    Update CustomUserDetailsService.java

--- src/main/java/org/godigit/policyvault/service/impl/CustomUserDetailsService.java
+++ src/main/java/org/godigit/policyvault/service/impl/CustomUserDetailsService.java
@@ -2,15 +2,16 @@ package org.godigit.policyvault.service.impl;
 
 import java.util.Set;
 import java.util.stream.Collectors;
-
-import org.springframework.security.core.authority.SimpleGrantedAuthority;
 import org.godigit.policyvault.entities.Users;
+import org.godigit.policyvault.exception.UserNotFoundException;
 import org.godigit.policyvault.repository.UserRepository;
-import org.springframework.security.core.userdetails.*;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.stereotype.Service;
 
-@Service
-public class CustomUserDetailsService implements UserDetailsService{
+@Service public class CustomUserDetailsService implements UserDetailsService {
 
     private final UserRepository users;
 
@@ -18,25 +19,24 @@ public class CustomUserDetailsService implements UserDetailsService{
         this.users = users;
     }
 
-    public UserDetails loadUserByEmail(String email) throws UsernameNotFoundException {
-        Users u = null;
-        try {
-            u = users.findByEmail(email)
-                    .orElseThrow(() -> new UsernameNotFoundException("User not found"));
-        } catch (Throwable e) {
-            throw new RuntimeException(e);
-        }
+    @Override
+    public UserDetails loadUserByUsername(String username) {
+        Users u = users.findByUsername(username)
+                .orElseThrow(() -> new UserNotFoundException("User with username '" + username + "' not found"));
+
         Set<SimpleGrantedAuthority> authorities = u.getRoles().stream()
                 .map(Enum::name)
                 .map(SimpleGrantedAuthority::new)
                 .collect(Collectors.toSet());
-        return new User(
-                u.getUsername(), u.getPasswordHash(), u.isEnabled(),
-                true, true, true, authorities);
-    }
 
-    @Override
-    public UserDetails loadUserByUsername(String email) throws UsernameNotFoundException {
-        return loadUserByEmail(email);
+        return new User(
+                u.getUsername(),
+                u.getPasswordHash(),
+                u.isEnabled(),
+                true,
+                true,
+                true,
+                authorities
+        );
     }
 }
