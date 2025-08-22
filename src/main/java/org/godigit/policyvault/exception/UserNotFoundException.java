ae78575c1b373770de696f6ef7478eb767130545
konu2514 <manjunathkonu@gmail.com>
2025-08-22 18:16:44 +0530

    Update and rename NotFoundException.java to UserNotFoundException.java

---
+++ src/main/java/org/godigit/policyvault/exception/UserNotFoundException.java
@@ -0,0 +1,8 @@
+package org.godigit.policyvault.exception;
+
+public class UserNotFoundException extends RuntimeException{
+    public UserNotFoundException(String message) {
+        super(message);
+    }
+
+}
