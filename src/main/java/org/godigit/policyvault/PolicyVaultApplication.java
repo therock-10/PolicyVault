a060d2cabd7d6b8ca1a62a031eb6df34ac9d67e8
Siddharth.Bhatt <siddharthbhatt2003@gmail.com>
2025-08-24 18:43:49 +0530

    Notification system integrated

--- src/main/java/org/godigit/policyvault/PolicyVaultApplication.java
+++ src/main/java/org/godigit/policyvault/PolicyVaultApplication.java
@@ -2,8 +2,10 @@ package org.godigit.policyvault;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
 @SpringBootApplication
+@EnableJpaAuditing(auditorAwareRef = "auditorAware")
 public class PolicyVaultApplication {
 
 	public static void main(String[] args) {
